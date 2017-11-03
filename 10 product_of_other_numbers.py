837cbea1f154ab93bd7793de51ad5fad8f56eaa9
anguillanneuf <tianzih@outlook.com>
2017-11-03 17:36:12 -0400

    InterviewCake pratices

---
+++ 10 product_of_other_numbers.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def get_products_of_all_ints_except_at_index(ints):
+    products = []
+    for i,v in enumerate(ints):
+        temp = 1
+        for j in ints:
+            if j != v:
+                temp *=j
+        products.append(temp)
+        
+    return products
+
+print(get_products_of_all_ints_except_at_index([1,2,3,4]))
+
+
+def get_products_of_all_ints_except_at_index2(ints):
+    products = [1]*len(ints)
+    products1 = [1]*len(ints)
+    products2 = [1]*len(ints)
+    
+    for i in range(1,len(ints)):
+        products1[i] = products1[i-1]*ints[i-1]
+        
+    for i in range(-1, -len(ints)-1, -1):
+        products2[i] = products2[i+1]*ints[i+1]
+        
+    for i in range(len(ints)):
+        products[i] = products1[i]*products2[i]
+        
+    return products
+
+print(get_products_of_all_ints_except_at_index2([1,2,3,4]))
