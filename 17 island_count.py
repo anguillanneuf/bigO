df8b13b95b0014015016468ec1de10b77f844bca
Tianzi <tianzih@outlook.com>
2017-11-14 14:32:00 -0500

    Added iterative approach, DFS with a stack

--- 17 island_count.py
+++ 17 island_count.py
@@ -6,6 +6,8 @@ Created on Wed Nov  8 22:25:16 2017
 @author: tz
 """
 
+"""Recursive approach
+"""
 def search(mat, i, j, m, n):
   
   if i > -1 and i < m and j > -1 and j < n:
@@ -34,3 +36,47 @@ binaryMatrix = [ [0,    1,    0,    1,    0],
                          [1,    0,    1,    0,    1] ]
 
 print(get_number_of_islands(binaryMatrix))
+
+"""Iterative approach
+"""
+
+def dfs_helper(stack, binaryMatrix, i, j, n, m):
+    if i >= 0 and i < n and j >= 0 and j < m:
+        if binaryMatrix[i][j] == 1:
+            stack.append((i, j))
+    return stack
+
+def depth_first_search(binaryMatrix, i, j, n, m):
+    stack = []
+    stack.append((i,j))
+    
+    while len(stack) > 0:
+
+        i, j = stack.pop()
+        binaryMatrix[i][j] = 0
+        
+        dfs_helper(stack, binaryMatrix, i+1, j, n, m)
+        dfs_helper(stack, binaryMatrix, i-1, j, n, m)
+        dfs_helper(stack, binaryMatrix, i, j+1, n, m)
+        dfs_helper(stack, binaryMatrix, i, j-1, n, m)
+
+def get_number_of_islands_iterative(binaryMatrix):
+    count = 0
+    
+    n, m = len(binaryMatrix), len(binaryMatrix[0])
+    
+    for i in range(n):
+        for j in range(m):
+            if binaryMatrix[i][j] == 1:
+                count += 1
+                depth_first_search(binaryMatrix, i, j, n, m)             
+                
+    return count
+
+binaryMatrix = [ [0,    1,    0,    1,    0],
+                 [0,    0,    1,    1,    1],
+                 [1,    0,    0,    1,    0],
+                 [0,    1,    1,    0,    0],
+                 [1,    0,    1,    0,    1] ]
+
+print(get_number_of_islands_iterative(binaryMatrix))
