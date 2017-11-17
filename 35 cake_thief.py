a729091f3c17943f8417727233bcfb1351cad835
Tianzi <tianzih@outlook.com>
2017-11-17 14:09:29 -0500

    Solved the famous knapsack problem - cake thief

--- 35 cake_thief.py
+++ 35 cake_thief.py
@@ -6,29 +6,22 @@ Created on Thu Nov 16 15:56:07 2017
 """
 
 def max_duffel_bag_value(cake_tuples, capacity):
-    
-    
-    values = [0] * 600
-    
-    for weight, value in cake_tuples:
-        values[value] += 1
-        for i in range(value+1, len(values)):
-            
-            if weight <= capacity:
-                values[i] += values[i-value]
-            else:
-                exit
-            
-    print(values)
+  if capacity == 0: return 0
+  
+  cake_dict = {weight: value for (weight, value) in cake_tuples}
+  weights = [0] * capacity
 
-    for k in range(599, -1, -1):
-        if values[k] > 0:
-            return k
-        
-    return -1
+  for weight, value in sorted(cake_tuples):
+    weights[weight-1] += cake_dict.get(weight)
+    
+    for i in range(weight, len(weights)):
+      temp = weights[i-weight] + cake_dict.get(weight)
+      if temp > weights[i]:
+        weights[i] = temp
+#  print(weights)
+  return max(weights)
 
 cake_tuples = [(7, 160), (3, 90), (2, 15)]
-#cake_tuples = [(7,3), (3,1)]
 capacity    = 20
 
 print(max_duffel_bag_value(cake_tuples, capacity))
