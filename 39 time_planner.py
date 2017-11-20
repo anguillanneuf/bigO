653970fe43a61966ddb13c1ae55b39964a1050b1
Tianzi <tianzih@outlook.com>
2017-11-20 15:18:01 -0500

    Removed the wrong recursive approach

--- 39 time_planner.py
+++ 39 time_planner.py
@@ -4,29 +4,6 @@ Created on Fri Nov 17 16:19:23 2017
 
 @author: harrisot
 """
-
-def meeting_planner_helper(slotsA, slotsB, dur, ptrA, ptrB):  
-  if ptrA >= len(slotsA) or ptrB >= len(slotsB): 
-    return []
-  
-  startTime = max(slotsA[ptrA][0], slotsB[ptrB][0])
-  endTime = min(slotsA[ptrA][1], slotsB[ptrB][1])
-  
-  if endTime - startTime >= dur:
-    return [startTime, startTime + dur]
-  else:
-    if slotsA[ptrA][1] < slotsB[ptrB][0]:
-      return meeting_planner_helper(slotsA, slotsB, dur, ptrA+1, ptrB)
-    elif slotsA[ptrA][0] < slotsB[ptrB][1]:
-      return meeting_planner_helper(slotsA, slotsB, dur, ptrA, ptrB+1)
-    else:
-      return meeting_planner_helper(slotsA, slotsB, dur, ptrA+1, ptrB+1)
-    
-def meeting_planner(slotsA, slotsB, dur):
-  return meeting_planner_helper(slotsA, slotsB, dur, 0, 0)  
-
-
-# iterative
 def meetingPlanner(slotsA, slotsB, dur):
   ia = 0
   ib = 0
@@ -44,11 +21,8 @@ def meetingPlanner(slotsA, slotsB, dur):
 
   return []
 
-slotsA = [[0, 19], [20, 40], [140, 210]]
+slotsA = [[0, 1], [2, 40], [140, 210]]
 slotsB = [[1, 13], [35,40]]
 dur = 8
-#slotsA, slotsB, dur =  [[1,10]], [[2,3],[5,7]], 2
-
-print(meeting_planner(slotsA, slotsB, dur))
 print(meetingPlanner(slotsA, slotsB, dur))
   
