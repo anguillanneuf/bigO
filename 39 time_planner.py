ebc856f6a27e225ba99be4541361a3291a6a8119
Tianzi <tianzih@outlook.com>
2017-11-20 15:19:37 -0500

    Added correct recursive approach

--- 39 time_planner.py
+++ 39 time_planner.py
@@ -25,4 +25,25 @@ slotsA = [[0, 1], [2, 40], [140, 210]]
 slotsB = [[1, 13], [35,40]]
 dur = 8
 print(meetingPlanner(slotsA, slotsB, dur))
+
+# recursive approach
+def meeting_planner_helper(slotsA, slotsB, dur, ptrA, ptrB):  
+  if ptrA >= len(slotsA) or ptrB >= len(slotsB): 
+    return []
+  
+  startTime = max(slotsA[ptrA][0], slotsB[ptrB][0])
+  endTime = min(slotsA[ptrA][1], slotsB[ptrB][1])
+
+  if endTime - startTime < dur:
+    if slotsA[ptrA][1] < slotsB[ptrB][1]:
+      return meeting_planner_helper(slotsA, slotsB, dur, ptrA+1, ptrB)
+    else:
+      return meeting_planner_helper(slotsA, slotsB, dur, ptrA, ptrB+1)
+  else: 
+    return [startTime, startTime+dur]
+    
+def meeting_planner(slotsA, slotsB, dur):
+  return meeting_planner_helper(slotsA, slotsB, dur, 0, 0) 
+
+print(meeting_planner(slotsA, slotsB, dur))
   
