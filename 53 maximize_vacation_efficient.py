79c56c48c757dea6c09f1e4f45fec6bf89d98977
Tianzi <tianzih@outlook.com>
2017-11-29 16:59:43 -0500

    Properly use memo

--- 53 maximize_vacation_efficient.py
+++ 53 maximize_vacation_efficient.py
@@ -18,14 +18,15 @@ class Vacation:
     if week == num_weeks: 
       return 0
     
-    if self.memo[city][week] < 0: 
+    if self.memo[city][week] > 0: 
       return self.memo[city][week]
     
     for i in range(num_cities):
       if i == city or self.flights[city][i] == 1:
         vac = max(vac, self.days[week][i] + self.maximize_vacation(i, week+1))
-        
-    return vac
+        self.memo[city][week] = vac
+    
+    return self.memo[city][week]
 
 flights = [[0,1,1],
            [1,0,1],
@@ -42,4 +43,5 @@ days2   = [[7,0,0],
 vac1 = Vacation(flights, days1)
 vac2 = Vacation(flights, days2)
 print(vac1.maximize_vacation(0,0)) # expected 12
+print(vac1.memo)
 print(vac2.maximize_vacation(0,0)) # expected 21
