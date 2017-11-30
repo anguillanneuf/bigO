98835d4f57288c89d7c8195bbd7f3934f07d01fe
Tianzi <tianzih@outlook.com>
2017-11-30 16:55:02 -0500

    Misunderstood the question the first time, find which appears twice, not once

--- 60 which_appears_twice.py
+++ 60 which_appears_twice.py
@@ -5,10 +5,15 @@ Created on Thu Nov 30 16:42:19 2017
 @author: harrisot
 """
 
-def which_appears_twice(arr):
+def which_appears_once(arr):
   temp = 0
   for k in arr:
     temp ^= k
   return temp
   
-print(which_appears_twice([23,45,65,2,45,2,23]))
+def which_appears_twice(arr,n):
+  return int(sum(arr)-n*(n+1)/2)
+  
+
+print(which_appears_once([23,45,65,2,45,2,23]))
+print(which_appears_twice([1,2,3,4,5,4],5))
