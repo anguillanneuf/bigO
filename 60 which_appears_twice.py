ba198aadfe63c531dc274cacbce2d6d739679754
Tianzi <tianzih@outlook.com>
2017-11-30 16:44:43 -0500

    Cool bit trick using XOR

---
+++ 60 which_appears_twice.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 16:42:19 2017
+
+@author: harrisot
+"""
+
+def which_appears_twice(arr):
+  temp = 0
+  for k in arr:
+    temp ^= k
+  return temp
+  
+print(which_appears_twice([23,45,65,2,45,2,23]))
