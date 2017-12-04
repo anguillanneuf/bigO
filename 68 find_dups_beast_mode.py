6b32cdac606fdf546e518c01fd1e10d5947b9844
Tianzi <tianzih@outlook.com>
2017-12-04 17:24:01 -0500

    Beast mode solution

--- 68 find_dups_beast_mode.py
+++ 68 find_dups_beast_mode.py
@@ -5,19 +5,44 @@ Created on Sun Dec  3 21:57:08 2017
 
 @author: tz
 """
-class LinkedList:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
 
 def find_dups_beast_mode(arr):
+  """Beast mode
+  1. find cycle
+  2. calculate length of the cycle
+  3. get into cycle
+  4. find the beginning or end of the cycle"""
+  
+  n = len(arr)
+  curr = n
+  
+  # Step 1
+  for _ in range(n-1):
+    curr = arr[curr-1]
+    
+  mark = curr
+  pointer = arr[curr-1]
+  count = 1
+  
+  # Step 2
+  while mark != pointer:
+    pointer = arr[pointer-1]
+    count += 1
+  print("Length of the cycle: {}".format(count))
+  
+  i = n
+  j = n
+  # Step 3
+  for _ in range(count):
+    i = arr[i-1]
+z
+  # Step 4
+  while i!=j:
+    print(i,j)
+    i = arr[i-1]
+    j = arr[j-1]
+    
+  return i
 
-  for curr in arr:
 
-    if arr[abs(curr)-1]>0:
-        arr[abs(curr)-1] = -arr[abs(curr)-1]
-    else:
-      return abs(curr)
-
-print(find_dups_beast_mode([1,2,3,4,2]))
-print(find_dups_beast_mode([2,2,1,3]))
+print(find_dups_beast_mode([1,3,2,1,3]))
