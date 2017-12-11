ea87b7e46352eab4e560dfc7a3716d863e754596
Tianzi <tianzih@outlook.com>
2017-12-11 17:42:15 -0500

    Correct O(n^2) solution:

--- 75 circus_tower.py
+++ 75 circus_tower.py
@@ -7,27 +7,46 @@ Created on Sun Dec 10 21:38:27 2017
 """
 
 def circus_tower(pp):
-  """O(nlogn)+O(n) time
+  """O(n*n) time
   O(n) space
-  Maybe if I use a black red tree or an AVL tree, I can
-  reduce it down to O(n) time to create tree and O(n)
-  time to traverse the tree to create the output
   """
-  pp = sorted(pp) # do I need to write this?
+  pp = sorted(pp)
   tower = []
 
-  for i,v in enumerate(pp):
-    if i == 0: 
-      tower.append(pp[i])
-      continue
-    if v[1] > tower[-1][1]:
-      tower.append(pp[i])
+  for i in range(len(pp)):
+    left = pp[i][1]
+    right = max([k[1] for k in pp[i:]])
+    temp = []
+    
+    for j in range(len(pp)):
+      if left <= pp[j][1] <= right:
+        temp.append(pp[j])
+        left = pp[j][1]
+        
+    if len(temp) > len(tower):
+      tower = temp
+      
   return tower
 
 pp1 = [(65,100),(70,150),(56,90),(75,190),(60,95),(68,110)]
-print(circus_tower(pp1))
+print(circus_tower(pp1)) 
 
-pp1 = [(75,87),(65,90),(70,91),(71,82),(72,84),(73,85),(74,86)]
-print(circus_tower(pp1))
+pp2 = [(75,87),(65,90),(70,91),(71,82),(72,84),(73,85)]
+print(circus_tower(pp2)) 
 
-# how to search for sub sequences in an array in O(n^2) and O(nlogn) time
+def subsequence_search(arr):
+  best = []
+  for i in range(len(arr)):
+    left = arr[i]
+    right = max(arr[i:])
+    temp = []
+    for j in range(i,len(arr)):
+      if left <= arr[j] <= right:
+        temp.append(arr[j])
+        left = arr[j]
+    if len(best)<len(temp):
+      best = temp
+    
+  return best
+
+print(subsequence_search([90,91,81,82,83,74,85])) 
