cd990e4d5ed8c3188a1e88d9a813864ba6486f57
anguillanneuf <tianzih@outlook.com>
2017-12-13 17:13:28 -0500

    Updated comments

--- 75 circus_tower.py
+++ 75 circus_tower.py
@@ -23,7 +23,7 @@ def circus_tower(pp):
         tower = max(mem[i], tower, key=len)
     if cond is False:
       mem[i].append(pp[i])
-
+  
   return tower
 
 pp1 = [(65,100),(70,150),(56,90),(75,190),(60,95),(68,110)]
@@ -31,20 +31,3 @@ print(circus_tower(pp1))
 
 pp2 = [(75,87),(65,90),(70,91),(71,82),(72,84),(73,85)]
 print(circus_tower(pp2)) 
-
-def longest_subsequence(arr):
-  mem = [[] for _ in range(len(arr))]
-  best = []
-  
-  for i in range(len(arr)):
-    cond = False
-    for j in range(i): 
-      if arr[i] > mem[j][-1]:
-        cond = True
-        mem[i] = mem[j] + [arr[i]]
-        best = max(mem[i], best, key=len) # key=lambda i: len(i)
-    if cond is False:
-      mem[i].append(arr[i])
-  return best
-
-print(longest_subsequence([80,90,91,81,82,83,74,85]))
