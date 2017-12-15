9a1f093f2e477142a437b3ae799c9c711e3001a0
Tianzi <tianzih@outlook.com>
2017-12-15 15:27:29 -0500

    Rotate matrix in-place

--- 82 rotate_matrix.py
+++ 82 rotate_matrix.py
@@ -5,22 +5,35 @@ Created on Wed Dec 13 22:33:49 2017
 
 @author: tz
 """
-
-def rotate_matrix(mat):
-  """Challenge: do it in place"""
+def rotate(i,j,mat):
   n = len(mat)
-  output = [[0 for _ in range(n)] for _ in range(n)]
-  for i in range(n):
-    for j in range(n):
-      output[j][abs(2-i)]=mat[i][j]  
-  return output
+  rollover = mat[i][j]
+  
+  for k in range(4):
+    putaway = mat[j][abs(n-1-i)]
+    mat[j][abs(n-1-i)] = rollover
+    rollover = putaway
+    
+    i,j = j,abs(n-i-1)
+    
+def rotate_matrix(mat):
+  n = len(mat)
+  
+  if n < 2: 
+    return mat
+  
+  for i in range(n-1):
+    rotate(0,i,mat)
+    if n>=4 and i<n-2:
+      rotate(1,n-1-i,mat)
+      
+  return mat
 
 mat = [[3,2,1],
        [6,5,4],
        [9,8,7]]
 
 print(rotate_matrix(mat))
-
 """
 [[9,6,3],
  [8,5,2],
