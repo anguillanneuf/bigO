0ac883f9f3dc2c4123bab89ce841ec1694c15954
Tianzi <tianzih@outlook.com>
2017-12-15 16:20:05 -0500

    Still working on it

---
+++ 84 subsort.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 15:40:21 2017
+
+@author: harrisot
+"""
+
+def subsort(arr):
+
+  if len(arr)<2:
+    return
+  # use binary search to find m and n
+  # O(logN) time 
+  
+  return (m,n)
+
+arr = [1,2,4,7,10,11,7,12,6,7,16,18,19]
+print(subsort(arr))
