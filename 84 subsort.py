5bd51976101ba504bdd87a055f07f4f6ced2b20a
anguillanneuf <tianzih@outlook.com>
2017-12-16 11:46:31 -0500

    O(n) time, O(1) space, much more efficient

--- 84 subsort.py
+++ 84 subsort.py
@@ -4,25 +4,11 @@ Created on Fri Dec 15 15:40:21 2017
 
 @author: harrisot
 """
-def expand(i,j,arr):
-  if i==0 or j==len(arr)-1:
-    return (0,len(arr)-1)
-  
-  lmax=max([arr[w] for w in range(j)])
-  rmin=min([arr[w] for w in range(i,len(arr))])
-  
-  if lmax <= arr[j+1] and rmin >= arr[i-1]:
-    return (i,j)
-  
-  i0,j0=expand(i-1,j,arr)
-  i1,j1=expand(i,j+1,arr)
-  i2,j2=expand(i-1,j+1,arr)
-  
-  return min((i0,j0),(i1,j1),(i2,j2),key=lambda a:a[1]-a[0])
-
 
 def subsort(arr):
+  
   n=len(arr)
+  left=0
   
   for i in range(n):
     if i==0: 
@@ -30,7 +16,8 @@ def subsort(arr):
     if arr[i]<arr[i-1]:
       left=i
       break
-  
+    
+  right=n-1
   for j in range(n-1,-1,-1):
     if j==1:
       continue
@@ -38,7 +25,20 @@ def subsort(arr):
       right=j-1
       break
   
-  return expand(left,right,arr)
+  lmax=max([arr[w] for w in range(left-1,right+1)])
+  rmin=min([arr[w] for w in range(left,right+2)])
+  
+  for i in range(left-1,-1,-1):
+    if arr[i]<=rmin:
+      left = i+1
+      break
+    
+  for j in range(right+1,n,1):
+    if arr[j]>=lmax:
+      right=j-1
+      break
+  
+  return left,right
 
 
 arr = [1,2,4,7,10,11,7,12,6,7,16,18,19]
