b425c910a701adcbb719f5fcbff0937a3d3c313f
anguillanneuf <tianzih@outlook.com>
2017-12-16 17:11:15 -0500

    use global arr if desired output is arr

--- 86 parens.py
+++ 86 parens.py
@@ -5,16 +5,22 @@ Created on Sat Dec 16 16:28:11 2017
 
 @author: tz
 """
+global ans
+ans = []
+
 def helper(p,q,prefix,n):
+
   if p==q==3:
-    print(prefix)
+    ans.append(prefix)
 
   if p>=q and p<=n:
     helper(p+1,q,prefix+'(',n)
     helper(p,q+1,prefix+')',n)
 
 def parens(n):
+
   p = q = 0
   helper(p,q,'',n)
+  return ans
 
-parens(3)
+print(parens(3))
