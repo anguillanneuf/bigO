1c3d9ea03ac9bcf9501fee259fce2039fc9cc399
anguillanneuf <tianzih@outlook.com>
2017-12-15 22:12:45 -0500

    Need a way to update lj and ri individually

--- 84 subsort.py
+++ 84 subsort.py
@@ -6,13 +6,37 @@ Created on Fri Dec 15 15:40:21 2017
 """
 
 def subsort(arr):
+  n=len(arr)
+  lj=0
+  for i in range(n):
+    if i==0: 
+      continue
+    if arr[i]<arr[i-1]:
+      lj=i
+      break
+    
+  ri=n-1
+  for i in range(n-1,-1,-1):
+    if i==1:
+      continue
+    if arr[i-1]>arr[i]:
+      ri=i
+      break
 
-  if len(arr)<2:
-    return
-  # use binary search to find m and n
-  # O(logN) time 
+  lmax=max([arr[i] for i in range(ri)])
+  rmin=min([arr[i] for i in range(lj,len(arr))])
+  print(lj, ri, lmax, rmin)
   
-  return (m,n)
+  while lmax >= arr[ri] and rmin <= arr[lj]:
+      lj-=1
+      ri+=1
+      lmax = max([arr[i] for i in range(ri)])
+      rmin = min([arr[i] for i in range(lj,len(arr))])
+      print(lj, ri, lmax, rmin)
+
+  return (lj,ri)
+  
+
 
 arr = [1,2,4,7,10,11,7,12,6,7,16,18,19]
-print(subsort(arr))
+print(subsort(arr)) # 3,9
