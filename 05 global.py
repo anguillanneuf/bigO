7f4689e09ac8b091176fc9290c2cffc562f3875b
anguillanneuf <tianzih@outlook.com>
2017-12-18 11:21:11 -0500

    Use class instead of global for mutable vars

--- 05 global.py
+++ 05 global.py
@@ -1,14 +1,32 @@
+# python lists are mutable
 mem1 = ['a', 'b', 'c']
+
+# python global vars are mutable
 global mem2
 mem2 = 'a'
 
-def hello(m1):
+# python class properties are also mutable
+class Params():
+  mem3 = 'a'
+
+
+def hello(m1, p):
+    
+    m1[1] = 'alien'   # modifies mem1
+    
     global mem2
-    m1[1] = 'alien'
-    mem2 = 'alien'
+    mem2 = 'alien'    # modifies mem2 without calling mem2 as a param
+    
+    p.mem3 += 'alien' # modifies p.mem3
+    
 
 print(mem1)
 print(mem2)
-hello(mem1)
+p = Params()
+print(p.mem3)
+
+hello(mem1, p)
+
 print(mem1)
 print(mem2)
+print(p.mem3)
