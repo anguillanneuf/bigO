b41e51be1b9c3f870e4ef87038caaaabe07d071f
anguillanneuf <tianzih@outlook.com>
2017-12-18 10:26:50 -0500

    Reformat and comment

--- 00 permutations.py
+++ 00 permutations.py
@@ -1,14 +1,25 @@
-def permutation(s):
-    permutation_internal(s, '')
+'''
+Find all the permutations of a string with no duplicated letters
 
+'''
+      
+def permutation(s, prefix=None):
+  
+  if len(s)==0:
+    print(prefix)
+    
+  else:
+    for i in range(len(s)):
+      rem = s[:i]+s[i+1:]
+      permutation(rem, prefix+s[i])
 
-def permutation_internal(s, prefix):
-    if len(s) == 0:
-        print(prefix)
-    else:
-        for i in range(len(s)):
-            rem = s[:i] + s[i + 1:]
-            permutation_internal(rem, prefix + s[i])
+permutation("cat", prefix="")
 
-
-permutation("cat")
+'''Print:
+cat
+cta
+act
+atc
+tca
+tac
+'''
