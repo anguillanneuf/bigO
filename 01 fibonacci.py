5e2f9d29250e094dbba75ba136ebd06240299f0d
anguillanneuf <tianzih@outlook.com>
2017-12-18 11:07:11 -0500

    Added comments. multiple recursive calls generally mean exponential runtime, branches^depth

--- 01 fibonacci.py
+++ 01 fibonacci.py
@@ -1,22 +1,23 @@
 def allFib(n):
+    # O(2^n) time despite for loop: 2^1+2^2+2^3+..+2^n=2^(n+1)
     mem = [0 for i in range(n)]
     for i in range(n):
-        print('{}:{}'.format(i, fib_internal(i, mem)))
-    print(mem)
-
+        print(fib_internal(i, mem))
 
 def fib_internal(n, mem):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n <= 1:
+        mem[n]=n
+        return n
     elif mem[n] > 0:
-        return mem[n]  # commenting this line results in +0.03 seconds in compute time
+        return mem[n]
 
     mem[n] = fib_internal(n - 1, mem) + fib_internal(n - 2, mem)
     return mem[n]
 
+# Recursion with memory
 allFib(10)
+
+
 
 def get_fib(position):
     if position < 0:
@@ -25,9 +26,15 @@ def get_fib(position):
         return position
     else:
         return get_fib(position-1) + get_fib(position-2)
+  
+# Recursion without memory
+print("Nth Fibonacci: {}".format(get_fib(10)))
+
+
 
 def fib_binet(n):
-    return int(1/(5**0.5)*(((1+5**0.5)/2)**n-((1-5**0.5)/2)**n))
+    return int( 1/(5**0.5) * \
+               (((1+5**0.5)/2)**n - ((1-5**0.5)/2)**n) )
 
-print(get_fib(10))
-print(fib_binet(10))
+# O(1) solution
+print("Nth Fibonacci: {}".format(fib_binet(10)))
