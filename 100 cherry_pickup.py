8d3e7d355e7c0e924dab4bf3b57b171fb96d3ce6
anguillanneuf <tianzih@outlook.com>
2018-01-05 20:53:42 -0500

    Provided solution

--- 100 cherry_pickup.py
+++ 100 cherry_pickup.py
@@ -18,85 +18,66 @@ The total number of cherries picked up is 5, and this is the maximum possible.
 
 """
 
-class Solution:
-  def cherryPickup(self, grid):
-    """
-    :type grid: List[List[int]]
-    :rtype: int
-    """
-    n = len(grid)
-    i=j=0
-    stack = dict()
-    path = [(i,j)]
-    stack.update({(i,j): [0 if grid[i][j]==0 else 1, path]})
-    
-    cherries = 0
-    temp_paths = []
-    
-    # (0,0) to (n-1,n-1)
-    while stack:
-      
-      (i,j),(c,path) = stack.popitem()
-      path_copy = path.copy()
-      
-      if i==n-1 and j==n-1:
-        temp_paths.append([c,path])
-      
-      if j+1<n and grid[i][j+1]!=-1:
-        path.append((i,j+1))
-        stack.update({(i,j+1): [max(c+grid[i][j+1], stack[(i,j+1)][0] if (i,j+1) in stack.keys() else 0),
-                                path]})
-  
-      if i+1<n and grid[i+1][j]!=-1:
-        path_copy.append((i+1,j))
-        stack.update({(i+1,j): [max(c+grid[i+1][j], stack[(i+1,j)][0] if (i+1,j) in stack.keys() else 0),
-                                path_copy]})
-
-    grid_copy = grid.copy()
-    
-    for cherry_path in temp_paths:
-
-      for (i,j) in cherry_path[1]:
-        grid[i][j]=0
-      
-      i=j=n-1
-      stack.update({(i,j):[cherry_path[0], cherry_path[1]]})
-      
-      while stack:
-        (i,j),(c,path) = stack.popitem()
-        path_copy = path.copy()
+class Solution(object):
+    def cherryPickup(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         
-        if i==0 and j==0:
-          if c > cherries:
-            cherries = c
+        if grid[0][0]==-1: return 0
         
-        if j-1>=0 and grid[i][j-1]!=-1:
-          path.append((i,j-1))
-          stack.update({(i,j-1): [max(c+grid[i][j-1], stack[(i,j-1)][0] if (i,j-1) in stack.keys() else 0),
-                                  path]})
-    
-        if i-1>=0 and grid[i-1][j]!=-1:
-          path_copy.append((i-1,j))
-          stack.update({(i-1,j): [max(c+grid[i-1][j], stack[(i-1,j)][0] if (i-1,j) in stack.keys() else 0),
-                                  path_copy]})
+        self.grid = grid
+        self.memo = {}
+        self.N = len(grid)
+        ans = max(self.helper(0,0,0,0), 0)
+#        print(self.memo)
+        return ans
 
-      grid = grid_copy.copy()
-  
 
-    return cherries
+    def helper(self,i1,j1,i2,j2):
+        
+        if (i1,j1,i2,j2) in self.memo: return self.memo[(i1,j1,i2,j2)]
+        
+        N = self.N
+        if i1 == N or j1 == N or i2 == N or j2 == N: return -1
+        if i1 == N-1 and j1 == N-1 and i2 == N-1 and j2 == N-1: return self.grid[-1][-1]
+        if self.grid[i1][j1] == -1 or self.grid[i2][j2] == -1: return -1
+        
+        # now can take a step in two directions at each end, which amounts to 4 combinations in total
+        dd = self.helper(i1+1, j1, i2+1, j2)
+        dr = self.helper(i1+1, j1, i2, j2+1)
+        rd = self.helper(i1, j1+1, i2+1, j2)
+        rr = self.helper(i1, j1+1, i2, j2+1)
+        maxComb = max([dd, dr, rd, rr])
+        
+        # find if there is a way to reach the end
+        if maxComb == -1:
+            out = -1
+        else:
+            # same cell, can only count this cell once
+            if i1 == i2 and j1 == j2:
+                out = maxComb + self.grid[i1][j1]
+            # different cell, can collect both
+            else:
+                out = maxComb + self.grid[i1][j1] + self.grid[i2][j2]
+                
+        # cache result
+        self.memo[(i1, j1, i2, j2)] = out
+        return out
     
     
 grid = [[0, 1, -1],
         [1, 0, -1],
         [1, 1,  1]]  
 
-grid = [[1,1,1,1,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,0,1,0,0,1],
-        [1,0,0,1,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,0,1,1,1,1]]
+#grid = [[1,1,1,1,0,0,0],
+#        [0,0,0,1,0,0,0],
+#        [0,0,0,1,0,0,1],
+#        [1,0,0,1,0,0,0],
+#        [0,0,0,1,0,0,0],
+#        [0,0,0,1,0,0,0],
+#        [0,0,0,1,1,1,1]]
         
 mysolution = Solution()
 print(mysolution.cherryPickup(grid))
