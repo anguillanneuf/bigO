58aa4c148e11344b4732e479e2edbc2f3d525234
anguillanneuf <tianzih@outlook.com>
2018-01-07 18:53:47 -0500

    Recursive solution O(n^3) space and 4^n time?

--- 100 cherry_pickup.py
+++ 100 cherry_pickup.py
@@ -12,72 +12,78 @@ Input: grid =
 Output: 5
 Explanation: 
 The player started at (0, 0) and went down, down, right right to reach (2, 2).
-4 cherries were picked up during this single trip, and the matrix becomes [[0,1,-1],[0,0,-1],[0,0,0]].
+4 cherries were picked up during this single trip, and the matrix becomes 
+[[0,1,-1],
+ [0,0,-1],
+ [0,0,0]].
 Then, the player went left, up, up, left to return home, picking up one more cherry.
 The total number of cherries picked up is 5, and this is the maximum possible.
-
 """
 
 class Solution(object):
     def cherryPickup(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        
-        if grid[0][0]==-1: return 0
-        
-        self.grid = grid
         self.memo = {}
-        self.N = len(grid)
-        ans = max(self.helper(0,0,0,0), 0)
-#        print(self.memo)
-        return ans
-
-
-    def helper(self,i1,j1,i2,j2):
+        self.grid = grid
+        return max(self.dp(0,0,0,0),0)
+    
+    def dp(self,i,j,p,q):
+        """Given two starting cells (i,j) and (p,q), return the max # 
+        of cherries from them to (n-1,n-1)
+        """
         
-        if (i1,j1,i2,j2) in self.memo: return self.memo[(i1,j1,i2,j2)]
+        if (i,j,p,q) in self.memo: return self.memo[(i,j,p,q)]
         
-        N = self.N
-        if i1 == N or j1 == N or i2 == N or j2 == N: return -1
-        if i1 == N-1 and j1 == N-1 and i2 == N-1 and j2 == N-1: return self.grid[-1][-1]
-        if self.grid[i1][j1] == -1 or self.grid[i2][j2] == -1: return -1
+        N = len(self.grid)
+        if any(o==N for o in [i,j,p,q]): return -1                     # out of bound
         
-        # now can take a step in two directions at each end, which amounts to 4 combinations in total
-        dd = self.helper(i1+1, j1, i2+1, j2)
-        dr = self.helper(i1+1, j1, i2, j2+1)
-        rd = self.helper(i1, j1+1, i2+1, j2)
-        rr = self.helper(i1, j1+1, i2, j2+1)
-        maxComb = max([dd, dr, rd, rr])
+        if all(o==N-1 for o in [i,j,p,q]): return self.grid[N-1][N-1]  # reach destination
+        # Note: in order for this to condition to ever be true
+        # (i,j) and (p,q) must be equally distant from (n-1,n-1)
+        # Feel free to change line 27 to max(self.dp(0,1,1,0),0) and it will work
+        # However, if i+j!=p+q, the result will not be satisfactory
         
-        # find if there is a way to reach the end
-        if maxComb == -1:
+        if self.grid[i][j]==-1 or self.grid[p][q]==-1: return -1       # hit a thorn
+        
+        dd = self.dp(i+1,j,p+1,q)
+        dr = self.dp(i+1,j,p,q+1)
+        rd = self.dp(i,j+1,p+1,q)
+        rr = self.dp(i,j+1,p,q+1)
+        
+        maxCherries = max(dd, dr, rd, rr)
+        
+        if maxCherries == -1:
             out = -1
         else:
-            # same cell, can only count this cell once
-            if i1 == i2 and j1 == j2:
-                out = maxComb + self.grid[i1][j1]
-            # different cell, can collect both
+            if i==p and j==q: # same cell
+                out = maxCherries + self.grid[i][j]
             else:
-                out = maxComb + self.grid[i1][j1] + self.grid[i2][j2]
-                
-        # cache result
-        self.memo[(i1, j1, i2, j2)] = out
+                out = maxCherries + self.grid[i][j] + self.grid[p][q]
+        
+        """memo stores starting positions and the max # of cherries given such 
+        starting positions
+        """
+        self.memo.update({(i,j,p,q):out})
         return out
     
     
-grid = [[0, 1, -1],
-        [1, 0, -1],
-        [1, 1,  1]]  
+grid1 = [[0, 1, -1],
+         [1, 0, -1],
+         [1, 1,  1]]  
 
-#grid = [[1,1,1,1,0,0,0],
-#        [0,0,0,1,0,0,0],
-#        [0,0,0,1,0,0,1],
-#        [1,0,0,1,0,0,0],
-#        [0,0,0,1,0,0,0],
-#        [0,0,0,1,0,0,0],
-#        [0,0,0,1,1,1,1]]
+grid2 = [[1,1,0],
+         [0,1,0],
+         [1,0,0]]
+
+grid3 = [[1,1,1,1,0,0,0],
+         [0,0,0,1,0,0,0],
+         [0,0,0,1,0,0,1],
+         [1,0,0,1,0,0,0],
+         [0,0,0,1,0,0,0],
+         [0,0,0,1,0,0,0],
+         [0,0,0,1,1,1,1]]
         
 mysolution = Solution()
-print(mysolution.cherryPickup(grid))
+print(mysolution.cherryPickup(grid1))
+print(mysolution.cherryPickup(grid3))
+print(mysolution.cherryPickup(grid2))
+print(mysolution.memo)
