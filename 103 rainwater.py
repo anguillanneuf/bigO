ea22e4e6caa5b62af95847b14ec17f426abbbc25
anguillanneuf <tianzih@outlook.com>
2018-01-14 20:29:19 -0500

    commented out print

--- 103 rainwater.py
+++ 103 rainwater.py
@@ -12,24 +12,28 @@ def collect_rainwater(arr):
     
     if len(arr)<3: return unit
     
-    i = 1
+    k = 1
     original = arr.copy()
     
-    while i < len(original):
+    while k + 1 < len(original):
         
-        if arr[i-1] < arr[i]:
-            i += 1
+        if arr[k-1] < arr[k]:
+            k += 1
         else:
+            i = k+1
             peak = max(arr[i:])
             j = i + arr[i:].index(peak)
-            arr[i:j+1] = [peak]*(j+1-i)
-            i = j+1
-
-    for i in range(len(original)):
-        unit += arr[i]-original[i]
+            arr[k:j] = [peak]*(j-k)
+            k = j
+            
+    #print(original)
+    #print(arr)
+            
+    for k in range(len(original)):
+        unit += arr[k]-original[k]
     
     return unit
 
 
-arr = [1,4,1,2,3,1,2,3,0,2]
+arr = [1,4,1,2,3,3,2,3,0,2]
 print(collect_rainwater(arr))
