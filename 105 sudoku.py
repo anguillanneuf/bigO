4d98e7f1d089531d4790ecf59543913642494e43
anguillanneuf <tianzih@outlook.com>
2018-01-15 20:46:27 -0500

    Found where the problem was

--- 105 sudoku.py
+++ 105 sudoku.py
@@ -35,32 +35,49 @@ def update_cell(cell, board):
   
     return cell
 
+def print_board(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j]=='.': pass
+    else: 
+        for row in board: print(row)
+            
 def sudoku_solve(board):
 
     empties = [update_cell([i,j], board) for i in range(9) for j in range(9) if board[i][j]=='.']
     empties = sorted(empties, key=len, reverse=True)
-
-#    print("empties:", empties[-5:])
     
-    while empties: 
+    # Exhausts simple choices
+    while len(empties[-1])<=3: 
+        
         empty = empties.pop()
         i,j = empty[0], empty[1]
         
         if len(empty)==2:
+            print("no ans", empty)
             return False
         
         if len(empty)==3:
             board[i][j]=empty[2]
             empties = [update_cell(cell[:2], board) for cell in empties]
             empties = sorted(empties, key=len, reverse=True)
-        
-        if len(empty)>3:
-            for v in empty[2:]:
-                board[i][j] = v
-                if sudoku_solve(board):
-                    return True
-                board[i][j] = '.'
-    return True
+    print("{} unfilled".format(len(empties)))      
+    if len(empties)==0: 
+        print_board(board)
+        return True
+       
+    # Deals with multiple choices
+    empty = empties.pop()
+    i,j = empty[0], empty[1]
+    votes = []
+    for v in empty[2:]:
+        board[i][j] = v
+        votes.append(sudoku_solve(board))
+        board[i][j] = '.'
+
+    if any(votes):
+        return True
+    return False
 
 board1 = [[".","8","9",".","4",".","6",".","5"],
           [".","7",".",".",".","8",".","4","1"],
@@ -84,6 +101,5 @@ board2 = [[".",".",".","7",".",".","3",".","1"],
           [".",".","6",".","2","1",".","5","."],
           [".",".",".",".",".","9",".",".","8"],
           ["8",".","5",".",".","4",".",".","."]]
-
 
 print(sudoku_solve(board2))
