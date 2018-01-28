5e5e3b1330d8b9f8df6de74af6a7b8bd7d261ff4
anguillanneuf <tianzih@outlook.com>
2018-01-28 12:56:55 -0500

    Implemented bit get, set, clear, update

---
+++ 121 bit.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 12:17:51 2018
+
+@author: tz
+"""
+
+class Bit(object):
+    
+    def __init__(self,v):
+        self.v = v
+        
+    def print_binary(self):
+        print(format(self.v, '#010b'))
+        
+    def get_bit(self, k):
+        mask = (1<<k)
+        return 1 if self.v & (mask) != 0 else 0
+    
+    def set_bit(self, k):
+        self.v |= (1 << k)
+    
+    def clear_bit(self, k):
+        # Method 1:
+#        self.v &= (-1^(1<<k))
+        # Method 2:
+        self.v &= ~(1 << k)
+    
+    def clear_bit_above(self, k): 
+        # kth to the most significant bit (inclusive)
+        self.v &= (1 << k) - 1
+    
+    def clear_bit_below(self, k):
+        # kth to the least significant bit (inclusive)
+        self.v &= (-1 << (k + 1))
+    
+    def update_bit(self, k, bit_value=0):
+        self.v |= (bit_value << k)
+
+
+myBit = Bit(59)
+myBit.print_binary()
+    
+myBit.set_bit(2)
+myBit.print_binary()
+
+myBit.clear_bit(2)
+myBit.print_binary()
+
+myBit.clear_bit_above(4)
+myBit.print_binary()
+
+myBit.clear_bit_below(2)
+myBit.print_binary()
+
+myBit.update_bit(7, 1)
+myBit.print_binary()
+
+
