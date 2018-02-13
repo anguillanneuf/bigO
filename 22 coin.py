456046f10ce838fca6049f30b00e7eadb6a983b3
anguillanneuf <tianzih@outlook.com>
2018-02-13 14:25:19 -0500

    Second time through, simpler code'

--- 22 coin.py
+++ 22 coin.py
@@ -64,5 +64,43 @@ def change_possibilities_bottom_up(amount, denominations):
 
 print(change_possibilities_bottom_up(8, [1,2,3]))
 
+def coins(amount, c):
+    # top down recursion
+    global cnt
+    cnt = 0
+    
+    def _coins(rem, i, c):
+        global cnt
+        if rem == 0:
+            cnt+=1
+            return # return is a formal exit
+        
+        if i <= len(c)-1:
+            K = rem//c[i]
+            for k in range(K, -1, -1):
+                _coins(rem-k*c[i], i+1, c)
+                
+    _coins(amount, 0, c)
+    
+    return cnt
+
+
+def coins_it(amount, c):
+    # bottom up iterative
+    mem = [0 for _ in range(amount+1)]
+    
+    for v in sorted(c):
+        
+        for u in range(v, amount+1):
+            if u == v:
+                mem[v] += 1
+            else:
+                mem[u] += mem[u-v]
+    
+    return mem[amount]
+
+print(coins(10, [5,2,1]))
+print(coins_it(10, [5,2,1]))
+
 
 
