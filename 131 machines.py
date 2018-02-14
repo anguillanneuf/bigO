8fcff81655f4c3974d937082bca16f9597202a56
anguillanneuf <tianzih@outlook.com>
2018-02-13 22:31:26 -0500

    Shortened code and added comments

--- 131 machines.py
+++ 131 machines.py
@@ -9,37 +9,35 @@ Created on Tue Feb 13 16:48:25 2018
 from collections import Counter
 
 def find_max_output(G):
-    candidates = []
-    for machine in G['machines']:
-        if len(G['machines']) == 1:
-            candidates.append(machine)
-            break
-        for e in G['machines'][machine]['input']:
-            if e.find('m') >= 0:
-                candidates.append(machine) 
-    candidates = list(set(candidates))
+    
+    # Step 1: candidates for final machine
+    
+    candidates = set(G['machines'].keys())
+    for machine in G['machines']:        
+        if not any(e.find('m') >=0 for e in G['machines'][machine]['input']):
+            candidates.remove(machine)
+    candidates = list(candidates)
+    
+    # Step 2: search for final machine in candidates queue
     
     while candidates:
         candidate = candidates.pop(0)
-        ms = []
-        for e in G['machines'][candidate]['input']:
-            if e.find('m') >= 0:
-                ms.append(e)
-                
+        ms = [e for e in G['machines'][candidate]['input'] if e.find('m') >= 0] 
         c = len(ms)
-        for m in ms:
-            mv = G['machines'][candidate]['input'][m]
+        
+        for m in ms:            
             if m not in candidates:
                 c -= 1
+                mv = G['machines'][candidate]['input'][m]
                 G['machines'][candidate]['input'].pop(m)
                 G_t1 = Counter(G['machines'][candidate]['input'])
                 G_t2 = Counter(G['machines'][m]['input'])
-                
+    
                 for _ in range(mv):
                     G_t1 += G_t2
             
                 G['machines'][candidate]['input'] = dict(G_t1)
-                    
+        print(candidate, c)           
         if c > 0:
             candidates.append(candidate)
             
@@ -52,8 +50,9 @@ def find_max_output(G):
 
 G = {'sources': {'s1': 90, 's2': 130, 's3': 110}, 
      'machines': {'m1': {'input': {'s1': 4, 's2': 2}, 'output': 1},
-                  'm2': {'input': {'s3': 5}, 'output': 1},
-                  'm3': {'input': {'m1': 1, 'm2': 2, 's2': 5}, 'output': 1}}}
+                  'm2': {'input': {'s3': 5, 'm4': 2}, 'output': 1},
+                  'm3': {'input': {'m1': 1, 'm2': 2, 's2': 5}, 'output': 1},
+                  'm4': {'input': {'s2': 1}, 'output': 1}}}
 
 
 print(find_max_output(G))
